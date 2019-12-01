c02cd7f9e31172aa541e03d06df83a2075c09d5b
HidekiTakazawa <ht0428a30@gmail.com>
2019-12-01 20:11:57 +0900

    Cloud source repositories test

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 from google.appengine.ext.db import Key
-
+######### 2019/12/01 Cloud Source Repositories テスト
 # DWW
 import os
 from google.appengine.ext.webapp import template
